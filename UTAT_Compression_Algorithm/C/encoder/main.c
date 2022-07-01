b6735e8d3920f1115fe769651fd8360b6699af12
Yong Da Li <yongdali314@gmail.com>
2022-07-01 01:30:49 -0400

    multiple encode working and manually checked to be correct. But running into issues with multiple decode.

--- UTAT_Compression_Algorithm/C/encoder/main.c
+++ UTAT_Compression_Algorithm/C/encoder/main.c
@@ -12,23 +12,24 @@
 #include <time.h>
 #include <gsl/gsl_matrix.h>
 
+#include "bitfile/bitfile.h"
 #include "main.h"
 #include "encoder.h"
 
 /**
- * creates a gsl matrix object with given size, filled with random integers from 0-99
+ * creates a gsl matrix object with given size, filled with random integers from 0-range
  * @param  nrow [number of rows]
  * @param  ncol [number of columns]
  * @return      [gsl matrix object, integers]
  */
-gsl_matrix_int* init_gsl_matrix(unsigned int nrow, unsigned int ncol){
+gsl_matrix_int* init_gsl_matrix(unsigned int nrow, unsigned int ncol, int range){
   int i, j; 
   srand( (unsigned int) time(NULL) );    // init random
-  gsl_matrix_int * matrix = gsl_matrix_int_alloc (nrow, ncol);
+  gsl_matrix_int* matrix = gsl_matrix_int_alloc (nrow, ncol);
 
   for (i = 0; i < nrow; i++){
     for (j = 0; j < ncol; j++){
-      gsl_matrix_int_set (matrix, i, j, (int) rand()%100);
+      gsl_matrix_int_set (matrix, i, j, (int) rand()%range);
       }
   }
 
@@ -36,6 +37,10 @@ gsl_matrix_int* init_gsl_matrix(unsigned int nrow, unsigned int ncol){
 }
 
 
+/**
+ * nicely prints out a gsl matrix (int only)
+ * @param matrix [gsl_matrix_int type]
+ */
 void pretty_print_matrix(gsl_matrix_int* matrix){
     int nrow = (int) matrix->size1;
     int ncol = (int) matrix->size2;
@@ -45,10 +50,31 @@ void pretty_print_matrix(gsl_matrix_int* matrix){
     int i, j;
     for (i=0; i<nrow; i++){
         for (j=0; j<ncol; j++){
-            printf("%d ", gsl_matrix_int_get(matrix, i, j));
+            printf("%3d ", gsl_matrix_int_get(matrix, i, j));  // %3d, at least 3 wide
         }
         printf("\n");
     }
+}
+
+void pretty_save_matrix(gsl_matrix_int* matrix, char* filepath){
+    FILE* fptr;
+    fptr = fopen(filepath, "w");
+
+    int nrow = (int) matrix->size1;
+    int ncol = (int) matrix->size2;
+    fprintf(fptr, "----- matrix ----- \n");
+    fprintf(fptr, "size: %d x %d\n", nrow, ncol);
+
+
+    int i, j;
+    for (i=0; i<nrow; i++){
+        for (j=0; j<ncol; j++){
+            fprintf(fptr, "%3d ", gsl_matrix_int_get(matrix, i, j));  // %3d, at least 3 wide
+        }
+        fprintf(fptr, "\n");
+    }
+
+    fclose(fptr);
 }
 
 
@@ -66,27 +92,8 @@ void check_single_encode(void){
 
 
     printf("---- encoding ----\n");
-    printf("sample: dec = %u, \tbin = "\
-        BYTE_TO_BINARY_PATTERN " " \
-        BYTE_TO_BINARY_PATTERN " " \
-        BYTE_TO_BINARY_PATTERN " " \
-        BYTE_TO_BINARY_PATTERN \
-        ", \thex = %x\n", sample, \
-        BYTE_TO_BINARY(sample>>24), \
-        BYTE_TO_BINARY(sample>>16), \
-        BYTE_TO_BINARY(sample>>8), \
-        BYTE_TO_BINARY(sample), sample);
-
-    printf("encoded: \t\tbin = "\
-        BYTE_TO_BINARY_PATTERN " " \
-        BYTE_TO_BINARY_PATTERN " " \
-        BYTE_TO_BINARY_PATTERN " " \
-        BYTE_TO_BINARY_PATTERN \
-        ", \thex = %x\n", \
-        BYTE_TO_BINARY(encoded>>24), \
-        BYTE_TO_BINARY(encoded>>16), \
-        BYTE_TO_BINARY(encoded>>8), \
-        BYTE_TO_BINARY(encoded), encoded);
+    print_binary_32(sample);
+    print_binary_32(encoded);
     printf("input sample uses %d bits\n", (int)sizeof(sample)*8);
     printf("encoded sample uses %d bits\n", num_bits_used);
 }
@@ -104,42 +111,11 @@ void check_single_decode(void){
     unsigned int k = 7;
 
     printf("===== decode =====\n");
-    printf("code: hex = %X, \tbin = "\
-        BYTE_TO_BINARY_PATTERN " " \
-        BYTE_TO_BINARY_PATTERN " " \
-        BYTE_TO_BINARY_PATTERN " " \
-        BYTE_TO_BINARY_PATTERN \
-        ", \thex = %x\n", code, \
-        BYTE_TO_BINARY(code>>24), \
-        BYTE_TO_BINARY(code>>16), \
-        BYTE_TO_BINARY(code>>8), \
-        BYTE_TO_BINARY(code), code);   
-
+    print_binary_32(code); 
     printf("decoded: %d\n", decode_sample(code, k));
 }
 
-
-void print_binary_32(uint32_t word){
-    printf("word: hex = %X, \tbin = "\
-        BYTE_TO_BINARY_PATTERN " " \
-        BYTE_TO_BINARY_PATTERN " " \
-        BYTE_TO_BINARY_PATTERN " " \
-        BYTE_TO_BINARY_PATTERN \
-        ", \thex = %x\n", word, \
-        BYTE_TO_BINARY(word>>24), \
-        BYTE_TO_BINARY(word>>16), \
-        BYTE_TO_BINARY(word>>8), \
-        BYTE_TO_BINARY(word), word);  
-}
-
-void check_multiple_encode_decode(void){
-    printf("==== muliple encode and decode ====\n");
-    int nrow = 5;
-    int ncol = 3;
-    gsl_matrix_int *matrix = init_gsl_matrix(nrow,ncol);
-   
-    // pretty_print_matrix(matrix);
-    
+void check_read_write_to_binary_file(void){
     uint32_t sample = 242;
     unsigned int k = 6;
     unsigned int num_bits_used;
@@ -149,7 +125,15 @@ void check_multiple_encode_decode(void){
     // write binary mode
     FILE *fptr;
     fptr = fopen("output/encoded.bin","wb");
-    fwrite(&code, 4, 1, fptr);
+    /**
+     * https://www.tutorialspoint.com/c_standard_library/c_function_fwrite.htm
+     * size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
+     * ptr − This is the pointer to the array of elements to be written.
+     * size − This is the size in bytes of each element to be written.
+     * nmemb − This is the number of elements, each one with a size of size bytes.
+     * stream − This is the pointer to a FILE object that specifies an output stream.
+     */
+    fwrite(&code, 1, sizeof(code), fptr);
     printf("written:\t");
     print_binary_32(code);
     // need to close the file in write mode, before trying to read it back
@@ -160,19 +144,91 @@ void check_multiple_encode_decode(void){
     // read binary mode
     fptr = fopen("output/encoded.bin", "rb");
     uint32_t read_code;
-    fread(&read_code, 4, 1, fptr);
+    fread(&read_code, 1, sizeof(read_code), fptr);
     printf("read:\t\t");
     print_binary_32(read_code);
     fclose(fptr);
+}
 
+void check_multiple_encode(int nrow, int ncol, int range, int k){
+    printf("==== muliple encode ====\n");
+
+    // setup gsl matrix with random data
+    gsl_matrix_int *matrix = init_gsl_matrix(nrow, ncol, range);
+    pretty_print_matrix(matrix);
+    pretty_save_matrix(matrix, "output/sample.txt");
+    
+    // setup encoding parameters
+    unsigned int num_bits_used;
+
+    // setup bitfile
+    FILE *outFile;
+    outFile = fopen("output/encoded.bin", "wb");
+
+    bit_file_t *bOutFile;               /* encoded output */
+    bOutFile = MakeBitFile(outFile, BF_WRITE);
+
+    int i, j;
+    uint32_t sample, code;
+    for(i=0; i<nrow; i++){
+        for(j=0; j<ncol; j++){
+            sample = gsl_matrix_int_get(matrix, i, j);
+            code = encode_sample_optimized(sample, k, &num_bits_used);
+            //  printf("(%d, %d): sample=%d, code=%x\n", i, j, sample, code);
+            BitFilePutBits(bOutFile, &code, num_bits_used);
+        }
+    }
+
+    /* pad fill with 1s so decode will run into EOF */
+    BitFileFlushOutput(bOutFile, 1);
 
     gsl_matrix_int_free(matrix);
 }
 
+
+/**
+ * TODO: Thursday, June 30, 2022
+ * multiple encode is manually checked to be correct
+ * but when decoding, no idea when the truncated binary portion ends
+ * and the unary part begins for the next character
+ *
+ * ex. 0xdec2 --> 0b1101111011000010
+ * samples: 94  66  60  94  30  81 ...
+ * k = 5 --> divisor = 32
+ *
+ * 0b11011110 11000010
+ * 94 = 2*32 + 30
+ *    = 110 + 11110
+ * 66 = 2*32 + 2
+ *    = 110 + 00010
+ *
+ * but the split happened manually, cuz I knew the correct input
+ */
+
+
+void check_multiple_decode(int nrow, int ncol, int k){
+    printf("==== muliple decode ====\n");
+
+    // setup bitfile
+    FILE *inFile;
+    inFile = fopen("output/encoded.bin", "rb");
+
+    bit_file_t *bInFile;               /* decoded input */
+    bInFile = MakeBitFile(inFile, BF_READ);
+
+}
+
 int main(void){
-    check_single_encode();
-    check_single_decode();
-    // check_multiple_encode_decode();
+    // check_single_encode();
+    // check_single_decode();
+    // check_read_write_to_binary_file();
+    
+    int nrow = 10;
+    int ncol = 10;
+    int range = 100;
+    int k = 5;
+    check_multiple_encode(nrow, ncol, range, k);
+    // check_multiple_decode(nrow, ncol, k);
     
     return 0;
 }
--- UTAT_Compression_Algorithm/C/encoder/main.h
+++ UTAT_Compression_Algorithm/C/encoder/main.h
@@ -16,4 +16,19 @@
 void check_single_encode(void);
 void check_single_decode(void);
 
+
+void print_binary_32(uint32_t word){
+    printf("word: hex = %X, \tbin = "\
+        BYTE_TO_BINARY_PATTERN " " \
+        BYTE_TO_BINARY_PATTERN " " \
+        BYTE_TO_BINARY_PATTERN " " \
+        BYTE_TO_BINARY_PATTERN \
+        ", \thex = %x\n", word, \
+        BYTE_TO_BINARY(word>>24), \
+        BYTE_TO_BINARY(word>>16), \
+        BYTE_TO_BINARY(word>>8), \
+        BYTE_TO_BINARY(word), word);  
+}
+
+
 #endif // MAIN_H
