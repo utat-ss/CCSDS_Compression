48017fdfc2e805bc8842101d0e84da6799dfaee1
Jasnoor <jasnoor.guliani@gmail.com>
2022-07-09 14:36:35 -0400

    Optimized parser to one loop through the file

--- UTAT_Compression_Algorithm/pass_data_into_C/main.c
+++ UTAT_Compression_Algorithm/pass_data_into_C/main.c
@@ -23,18 +23,18 @@ int main(int argc, char *argv[])
     }
 
     int num_row, num_col, num_frame;
-    gsl_matrix *dataCube;
+    gsl_matrix **dataCube;
     dataCube = parse(argv[1], &num_row, &num_col, &num_frame);
 
     int frame = 0;
-    gsl_matrix mat = dataCube[frame];
+    gsl_matrix *mat = dataCube[frame];
 
     printf("Printing frame %d\n", frame);
     for (int row = 0; row < num_row; row++)
     {
         for (int col = 0; col < num_col; col++)
         {
-            printf("\t%3.1f", gsl_matrix_get(&mat, 0, row * num_col + col));
+            printf("\t%3.1f", gsl_matrix_get(mat, 0, row * num_col + col));
         }
         printf("\n");
     }
